31763951e46b935ebdee403074fa37ebe9fc282e
conganh12345 <anhvca1234@gmail.com>
2025-04-05 16:43:52 +0700

    feat: set source v2

--- apps/cores/views.py
+++ apps/cores/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 
-# Create your views here.
 def index(request):
+    if not request.user.is_authenticated:
+       return redirect('/admin') 
     return render(request, 'dashboard/index.html')
