ac50c2838b760b9daf3984f56322258cffb1fd5b
conganh12345 <anhvca1234@gmail.com>
2025-04-05 16:49:59 +0700

    Revert "feat: set source v2"
    This reverts commit 31763951e46b935ebdee403074fa37ebe9fc282e.

--- apps/cores/views.py
+++ apps/cores/views.py
@@ -1,7 +1,6 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render
 from django.http import HttpResponse
 
+# Create your views here.
 def index(request):
-    if not request.user.is_authenticated:
-       return redirect('/admin') 
     return render(request, 'dashboard/index.html')
