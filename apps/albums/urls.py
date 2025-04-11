bc0e5a0c72a856cbfd200cc973ad2c641aca2429
phuc <nguyenvanminhphuc17@gmail.com>
2025-04-11 21:40:55 +0700

    DONE SONG-ALBUM

--- apps/albums/urls.py
+++ apps/albums/urls.py
@@ -7,4 +7,8 @@ urlpatterns = [
     path('create/', views.create_album, name='album_create'),
     path('edit/<int:album_id>/', views.edit_album, name='album_edit'),
     path('delete/<int:id>/', views.delete_album, name='album_delete'),
+    path('detail/<int:id>/', views.detail_album, name='detail_album'),
+    path('detail/delete/<int:id>/', views.delete_song_in_album, name='delete_song_in_album'),
+    path('detail/add/<int:song_id>/<int:album_id>', views.add_song_in_album, name='add_song_in_album'),
+
 ]
