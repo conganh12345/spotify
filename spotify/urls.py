4ac94e99f2083f48692b828f24a3bba8b2423918
conganh12345 <anhvca1234@gmail.com>
2025-05-11 22:38:18 +0700

    spotify/update-permissions

--- spotify/urls.py
+++ spotify/urls.py
@@ -19,6 +19,8 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 
+app_name = 'spotify'
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('apps.cores.urls')),  
